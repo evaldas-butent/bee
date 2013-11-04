22849b6adab60766967dfa36ceee9e09d3c125b3
Vidas <vidas@butent.com>
2013-11-04 20:37:12 +0000

    Updated settings
    git-svn-id: http://192.168.0.243:4435/svn/bee/branches/alterna@1283 aaee961a-53e7-2249-bd01-b2a8f0c4ce08

--- war/js/ecsettings.js
+++ war/js/ecsettings.js
@@ -1,8 +1,9 @@
 var BeeSettings = {
-  appName: "E-commerce",
-  logoImage: "images/ec/logo.png",
+  appName: "E-komercija",
   startMillis: new Date().getTime(),
   logCapacity: 1000,
   showGlobalSearch: false,
-  showItemSuppliers: false
+  showItemSuppliers: false,
+  northHeight: 190,
+  styleSheets: "ext/alterna/alterna.css"
 };
