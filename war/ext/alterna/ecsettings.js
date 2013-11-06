bb172cde3f29dd2c06fa7fdb0766aa4823391015
Marius <marius@butent.com>
2013-11-06 07:42:55 +0000

    e-commerce alterna logo
    git-svn-id: http://192.168.0.243:4435/svn/bee/trunk@1287 aaee961a-53e7-2249-bd01-b2a8f0c4ce08

--- war/ext/alterna/ecsettings.js
+++ war/ext/alterna/ecsettings.js
@@ -1,9 +1,10 @@
 var BeeSettings = {
   appName: "E-komercija",
+  logoImage: "ext/alterna/logo.gif",
   startMillis: new Date().getTime(),
   logCapacity: 1000,
   showGlobalSearch: false,
   showItemSuppliers: false,
   northHeight: 190,
-  styleSheets: "ext/alterna/alterna.css"
+  styleSheets: "ext/alterna/alterna.css?v=2"
 };
