3212b27359a82960e997771c050910f68b81c641
Evaldas <evaldas.bagatavicius@butent.lt>
2014-06-19 11:40:02 +0000

    Service module. Show main criteria in calendar of objects.
    git-svn-id: http://192.168.0.243:4435/svn/bee/trunk@1857 aaee961a-53e7-2249-bd01-b2a8f0c4ce08

--- src/com/butent/bee/client/modules/service/ServiceObjectWrapper.java
+++ src/com/butent/bee/client/modules/service/ServiceObjectWrapper.java
@@ -3,8 +3,18 @@ package com.butent.bee.client.modules.service;
 import static com.butent.bee.shared.modules.service.ServiceConstants.*;
 
 import com.butent.bee.client.data.Data;
+import com.butent.bee.client.data.Queries;
+import com.butent.bee.client.data.Queries.RowSetCallback;
+import com.butent.bee.client.layout.Flow;
 import com.butent.bee.client.timeboard.TimeBoardHelper;
+import com.butent.bee.shared.BeeConst;
+import com.butent.bee.shared.data.BeeRow;
+import com.butent.bee.shared.data.BeeRowSet;
+import com.butent.bee.shared.data.DataUtils;
 import com.butent.bee.shared.data.SimpleRowSet.SimpleRow;
+import com.butent.bee.shared.data.filter.Filter;
+import com.butent.bee.shared.i18n.Localized;
+import com.butent.bee.shared.utils.BeeUtils;
 
 class ServiceObjectWrapper {
 
@@ -19,16 +29,22 @@ class ServiceObjectWrapper {
   private static final String contractorLabel = Data.getColumnLabel(VIEW_SERVICE_OBJECTS,
       COL_SERVICE_CONTRACTOR);
 
+  private static final int MAX_MAIN_CRITERIA_COUNT = 20;
+
   private final Long id;
 
   private final String address;
 
   private final String title;
 
+  private final String mainCriteriaTitle;
+
   ServiceObjectWrapper(SimpleRow row) {
     this.id = row.getLong(idColumn);
 
     this.address = row.getValue(COL_SERVICE_ADDRESS);
+
+    this.mainCriteriaTitle = BeeConst.STRING_EMPTY;
 
     this.title = TimeBoardHelper.buildTitle(
         categoryLabel, row.getValue(ALS_SERVICE_CATEGORY_NAME),
@@ -47,5 +63,48 @@ class ServiceObjectWrapper {
 
   String getTitle() {
     return title;
+  }
+
+  String getMainCriteriaTitle() {
+    return this.mainCriteriaTitle;
+  }
+
+  void appendMainCriteriaTitle(final Flow panel) {
+    Filter filter = Filter.and(Filter.equals(COL_SERVICE_OBJECT, getId()),
+        Filter.isNull(COL_SERVICE_CRITERIA_GROUP_NAME));
+
+    Queries.getRowSet(VIEW_SERVICE_OBJECT_CRITERIA, null, filter, new RowSetCallback() {
+
+      @Override
+      public void onSuccess(BeeRowSet result) {
+        if (result.getNumberOfRows() < 0) {
+          return;
+        }
+
+        int i = 0;
+        for (BeeRow crit : result) {
+          String name = DataUtils.getString(result, crit, COL_SERVICE_CRITERION_NAME);
+
+          if (!BeeUtils.isEmpty(name)) {
+            String value = DataUtils.getString(result, crit, COL_SERVICE_CRITERION_VALUE);
+
+            value = BeeUtils.isEmpty(value) ? Localized.getConstants().filterNullLabel() : value;
+
+            panel.setTitle(BeeUtils.buildLines(panel.getTitle(), TimeBoardHelper.buildTitle(name,
+                value)));
+
+            i++;
+            int left = result.getNumberOfRows() - MAX_MAIN_CRITERIA_COUNT;
+            if ((i >= MAX_MAIN_CRITERIA_COUNT) && (left > 0)) {
+              panel.setTitle(BeeUtils.buildLines(panel.getTitle(), TimeBoardHelper.buildTitle(
+                  BeeConst.ELLIPSIS,
+                  BeeUtils.bracket(left))));
+              break;
+            }
+          }
+        }
+      }
+
+    });
   }
 }
