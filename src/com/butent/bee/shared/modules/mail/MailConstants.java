3ea928adb196a83b1bb43ea7e5407e522dfed9c5
Vidas <vidas@butent.com>
2014-09-19 08:48:38 +0000

    ConcurrencyBean Mail check synchronization
    git-svn-id: http://192.168.0.243:4435/svn/bee/trunk@2051 aaee961a-53e7-2249-bd01-b2a8f0c4ce08

--- src/com/butent/bee/shared/modules/mail/MailConstants.java
+++ src/com/butent/bee/shared/modules/mail/MailConstants.java
@@ -224,6 +224,7 @@ public final class MailConstants {
   public static final String DATA_TYPE_MESSAGE = "Message";
 
   public static final String PRM_DEFAULT_ACCOUNT = "DefaultAccount";
+  public static final String PRM_MAIL_CHECK_INTERVAL = "MailCheckIntervalInMinutes";
 
   public static void register() {
     EnumUtils.register(RuleCondition.class);
