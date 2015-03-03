a0610a9b47f56e2645548cce411a60e32522ef7e
Evaldas <evaldas.bagatavicius@butent.lt>
2015-03-03 13:06:53 +0000

    Standart contacts sub-module. Remove obsolete (unused) fields. Changes input field design. Adding family classifiers.
    git-svn-id: http://192.168.0.243:4435/svn/bee/trunk@2472 aaee961a-53e7-2249-bd01-b2a8f0c4ce08

--- src/com/butent/bee/shared/modules/tasks/TaskConstants.java
+++ src/com/butent/bee/shared/modules/tasks/TaskConstants.java
@@ -413,6 +413,7 @@ public final class TaskConstants {
 
   public static final String GRID_RELATED_TASKS = "RelatedTasks";
   public static final String GRID_CHILD_TASKS = "ChildTasks";
+  public static final String GRID_CHILD_REQUESTS = "ChildRequests";
   public static final String GRID_RELATED_RECURRING_TASKS = "RelatedRecurringTasks";
 
   public static final String GRID_REQUESTS = "Requests";
