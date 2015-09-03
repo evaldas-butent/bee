a2018e0af10e9f593e3c26253326b2a9abb9ffd2
vidas <vidas@butent.com>
2015-09-03 11:08:03 +0300

    RESTful webservice draft

---
+++ src/com/butent/bee/server/rest/EntryPoint.java
@@ -0,0 +1,11 @@
+package com.butent.bee.server.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class EntryPoint extends Application {
+
+  public static final String ENTRY = "entry";
+
+}
