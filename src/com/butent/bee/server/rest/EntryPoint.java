d4d38fb5e66225ba629c02962aae594c2b1d034f
vidas <vidas@butent.com>
2015-10-01 18:26:59 +0300

    Fixes after merge

--- src/com/butent/bee/server/rest/EntryPoint.java
+++ src/com/butent/bee/server/rest/EntryPoint.java
@@ -1,5 +1,7 @@
 package com.butent.bee.server.rest;
 
+import com.butent.bee.server.modules.tasks.RestfulTaskBuilder;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,6 +17,8 @@ public class EntryPoint extends Application {
 
     classes.add(AuthenticationFilter.class);
     classes.add(Worker.class);
+
+    classes.add(RestfulTaskBuilder.class);
 
     return classes;
   }
