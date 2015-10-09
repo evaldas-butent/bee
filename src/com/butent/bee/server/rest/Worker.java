c1f3097a9f7e437db87ef7900394dfadc5c93ae5
vidas <vidas@butent.com>
2015-10-09 14:30:43 +0300

    Licences

--- src/com/butent/bee/server/rest/Worker.java
+++ src/com/butent/bee/server/rest/Worker.java
@@ -4,15 +4,20 @@ import static com.butent.bee.shared.modules.classifiers.ClassifierConstants.*;
 
 import com.butent.bee.server.data.QueryServiceBean;
 import com.butent.bee.server.data.SystemBean;
+import com.butent.bee.server.rest.annotations.Trusted;
 import com.butent.bee.server.sql.SqlSelect;
+import com.butent.bee.server.sql.SqlUtils;
 import com.butent.bee.shared.BeeConst;
 import com.butent.bee.shared.data.SimpleRowSet;
+import com.butent.bee.shared.utils.BeeUtils;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -35,6 +40,24 @@ public class Worker {
         .addFrom(TBL_COMPANIES));
 
     return rowSetResponse(companies);
+  }
+
+  @GET
+  @Path("endpoint")
+  @Trusted(secret = "B-NOVO")
+  public String getPath(@HeaderParam("licence") String licence) {
+    String endpoint = null;
+
+    if (!BeeUtils.isEmpty(licence)) {
+      endpoint = qs.getValue(new SqlSelect()
+          .addFields(TBL_COMPANY_LICENCES, COL_LICENCE_ENDPOINT)
+          .addFrom(TBL_COMPANY_LICENCES)
+          .setWhere(SqlUtils.equals(TBL_COMPANY_LICENCES, COL_LICENCE, licence)));
+    }
+    if (BeeUtils.isEmpty(endpoint)) {
+      throw new NotFoundException(licence);
+    }
+    return endpoint;
   }
 
   private static Response rowSetResponse(SimpleRowSet rowSet) {
