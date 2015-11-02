beb9fc8fbd55963a6b0931dc01b0f9ce832d77d7
Marius <marius@butent.com>
2015-11-02 13:15:41 +0200

    extract common functionality to TreeGridInterceptor

--- src/com/butent/bee/client/modules/service/ServiceObjectsGrid.java
+++ src/com/butent/bee/client/modules/service/ServiceObjectsGrid.java
@@ -1,65 +1,20 @@
 package com.butent.bee.client.modules.service;
 
-import com.google.gwt.event.logical.shared.SelectionEvent;
-import com.google.gwt.event.logical.shared.SelectionHandler;
-
-import com.butent.bee.client.data.Data;
-import com.butent.bee.client.ui.IdentifiableWidget;
-import com.butent.bee.client.ui.FormFactory.WidgetDescriptionCallback;
-import com.butent.bee.client.view.TreeView;
 import com.butent.bee.client.view.grid.GridView;
-import com.butent.bee.client.view.grid.interceptor.AbstractGridInterceptor;
 import com.butent.bee.client.view.grid.interceptor.GridInterceptor;
-import com.butent.bee.shared.data.BeeRowSet;
-import com.butent.bee.shared.data.DataUtils;
+import com.butent.bee.client.view.grid.interceptor.TreeGridInterceptor;
 import com.butent.bee.shared.data.IsRow;
 import com.butent.bee.shared.data.filter.Filter;
 import com.butent.bee.shared.modules.service.ServiceConstants;
-import com.butent.bee.shared.ui.GridDescription;
 
-public class ServiceObjectsGrid extends AbstractGridInterceptor implements
-    SelectionHandler<IsRow> {
-
-  private TreeView categoryTree;
+public class ServiceObjectsGrid extends TreeGridInterceptor {
 
   ServiceObjectsGrid() {
   }
 
   @Override
-  public void afterCreateWidget(String name, IdentifiableWidget widget,
-      WidgetDescriptionCallback callback) {
-
-    if (widget instanceof TreeView) {
-      categoryTree = (TreeView) widget;
-      categoryTree.addSelectionHandler(this);
-    }
-  }
-
-  @Override
-  public BeeRowSet getInitialRowSet(GridDescription gridDescription) {
-    return Data.createRowSet(gridDescription.getViewName());
-  }
-
-  @Override
   public GridInterceptor getInstance() {
     return new ServiceObjectsGrid();
-  }
-
-  @Override
-  public void onSelection(SelectionEvent<IsRow> event) {
-    if (event != null) {
-      Long category = (event.getSelectedItem() == null) ? null : event.getSelectedItem().getId();
-      Filter flt;
-
-      if (category != null) {
-        flt = Filter.equals(ServiceConstants.COL_SERVICE_CATEGORY, category);
-      } else {
-        flt = Filter.isFalse();
-      }
-
-      getGridPresenter().getDataProvider().setDefaultParentFilter(flt);
-      getGridPresenter().refresh(true, true);
-    }
   }
 
   @Override
@@ -71,16 +26,24 @@ public class ServiceObjectsGrid extends AbstractGridInterceptor implements
       newRow.setValue(categoryIdx, oldRow.getString(categoryIdx));
       newRow.setValue(nameIdx, oldRow.getString(nameIdx));
 
-    } else if (categoryTree != null) {
-      IsRow category = categoryTree.getSelectedItem();
+    } else if (getTreeView() != null && getSelectedTreeItem() != null) {
+      IsRow category = getSelectedTreeItem();
 
       if (category != null) {
         newRow.setValue(categoryIdx, category.getId());
         newRow.setValue(nameIdx,
-            category.getString(DataUtils.getColumnIndex(ServiceConstants.COL_SERVICE_CATEGORY_NAME,
-                categoryTree.getTreePresenter().getDataColumns())));
+            category.getString(getTreeColumnIndex(ServiceConstants.COL_SERVICE_CATEGORY_NAME)));
       }
     }
     return true;
+  }
+
+  @Override
+  protected Filter getFilter(Long treeItemId) {
+    if (treeItemId != null) {
+      return Filter.equals(ServiceConstants.COL_SERVICE_CATEGORY, treeItemId);
+    } else {
+      return Filter.isFalse();
+    }
   }
 }
