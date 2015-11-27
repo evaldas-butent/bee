1ed017d564de1c5d653ed737afe094ee9eb6f6be
vidas <vidas@b-novo.com>
2015-11-27 22:42:34 +0200

    REST removed getApi

--- src/com/butent/bee/server/rest/Worker.java
+++ src/com/butent/bee/server/rest/Worker.java
@@ -3,20 +3,16 @@ package com.butent.bee.server.rest;
 import static com.butent.bee.server.rest.CrudWorker.*;
 import static com.butent.bee.shared.modules.administration.AdministrationConstants.*;
 import static com.butent.bee.shared.modules.classifiers.ClassifierConstants.COL_COMPANY_PERSON;
-import static com.butent.bee.shared.modules.documents.DocumentConstants.*;
 
 import com.butent.bee.server.data.QueryServiceBean;
 import com.butent.bee.server.data.SystemBean;
 import com.butent.bee.server.data.UserServiceBean;
 import com.butent.bee.server.modules.administration.FileStorageBean;
-import com.butent.bee.server.rest.annotations.Trusted;
 import com.butent.bee.server.sql.IsCondition;
 import com.butent.bee.server.sql.SqlSelect;
 import com.butent.bee.server.sql.SqlUtils;
 import com.butent.bee.shared.BeeConst;
 import com.butent.bee.shared.data.SimpleRowSet;
-import com.butent.bee.shared.utils.BeeUtils;
-import com.butent.bee.shared.utils.Codec;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -27,9 +23,7 @@ import java.util.Objects;
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -94,23 +88,5 @@ public class Worker {
   public Response login() {
     return Response.temporaryRedirect(UriBuilder.fromResource(CompanyPersonsWorker.class)
         .path("{id}").build(usr.getCompanyPerson(usr.getCurrentUserId()))).build();
-  }
-
-  @GET
-  @Path("{api}.pdf")
-  @Trusted
-  public Response getApi(@PathParam("api") String name) {
-    String content = qs.getValue(new SqlSelect()
-        .addFields(TBL_DOCUMENT_DATA, COL_DOCUMENT_CONTENT)
-        .addFrom(TBL_DOCUMENTS)
-        .addFromInner(TBL_DOCUMENT_DATA,
-            sys.joinTables(TBL_DOCUMENT_DATA, TBL_DOCUMENTS, COL_DOCUMENT_DATA))
-        .setWhere(SqlUtils.equals(TBL_DOCUMENTS, COL_DOCUMENT_NAME, name)));
-
-    if (BeeUtils.isEmpty(content)) {
-      throw new NotFoundException();
-    }
-    return new FileServiceApplication().getFile(name + ".pdf",
-        Codec.encodeBase64(fs.createPdf(content)));
   }
 }
