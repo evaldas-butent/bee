03ae966fa38765236b69ea15b3b8b113dea1c921
Vidas <vidas@b-novo.com>
2016-04-13 15:38:54 +0300

    TripCosts sum entry on amount

--- src/com/butent/bee/client/modules/transport/TripCostsGrid.java
+++ src/com/butent/bee/client/modules/transport/TripCostsGrid.java
@@ -10,29 +10,33 @@ import com.butent.bee.client.Global;
 import com.butent.bee.client.communication.ParameterList;
 import com.butent.bee.client.communication.ResponseCallback;
 import com.butent.bee.client.composite.DataSelector;
+import com.butent.bee.client.data.ClientDefaults;
 import com.butent.bee.client.data.Data;
-import com.butent.bee.client.dialog.InputCallback;
+import com.butent.bee.client.data.Queries;
+import com.butent.bee.client.data.RowUpdateCallback;
 import com.butent.bee.client.event.logical.ParentRowEvent;
 import com.butent.bee.client.event.logical.SelectorEvent;
 import com.butent.bee.client.layout.Flow;
 import com.butent.bee.client.presenter.GridPresenter;
-import com.butent.bee.client.view.add.ReadyForInsertEvent;
+import com.butent.bee.client.view.edit.EditStartEvent;
 import com.butent.bee.client.view.edit.Editor;
-import com.butent.bee.client.view.edit.ReadyForUpdateEvent;
-import com.butent.bee.client.view.grid.GridView;
 import com.butent.bee.client.view.grid.interceptor.AbstractGridInterceptor;
 import com.butent.bee.client.view.grid.interceptor.GridInterceptor;
 import com.butent.bee.client.widget.FaLabel;
 import com.butent.bee.client.widget.InputNumber;
-import com.butent.bee.shared.BeeConst;
-import com.butent.bee.shared.Consumer;
 import com.butent.bee.shared.communication.ResponseObject;
+import com.butent.bee.shared.data.BeeColumn;
 import com.butent.bee.shared.data.DataUtils;
+import com.butent.bee.shared.data.IsRow;
 import com.butent.bee.shared.data.filter.Filter;
 import com.butent.bee.shared.font.FontAwesome;
 import com.butent.bee.shared.i18n.Localized;
+import com.butent.bee.shared.modules.trade.Totalizer;
+import com.butent.bee.shared.modules.trade.TradeConstants;
 import com.butent.bee.shared.utils.BeeUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class TripCostsGrid extends AbstractGridInterceptor
@@ -109,62 +113,59 @@ public class TripCostsGrid extends AbstractGridInterceptor
   }
 
   @Override
-  public void onReadyForInsert(GridView gridView, ReadyForInsertEvent event) {
-    int idx = DataUtils.getColumnIndex(COL_COSTS_PRICE, event.getColumns());
-
-    if (!BeeConst.isUndef(idx)
-        && BeeUtils.isZero(BeeUtils.toDoubleOrNull(event.getValues().get(idx)))) {
-
+  public void onEditStart(EditStartEvent event) {
+    if (Objects.equals(event.getColumnId(), TradeConstants.VAR_TOTAL)) {
       event.consume();
-
-      amountEntry(BeeUtils.toDoubleOrNull(event.getValues()
-              .get(DataUtils.getColumnIndex(COL_COSTS_QUANTITY, event.getColumns()))),
-          (newPrice) -> {
-            event.getValues().set(idx, newPrice);
-            event.setConsumed(false);
-            gridView.fireEvent(event);
-          });
+      amountEntry(event.getRowValue());
       return;
     }
-    super.onReadyForInsert(gridView, event);
+    super.onEditStart(event);
   }
 
-  @Override
-  public void onReadyForUpdate(GridView gridView, ReadyForUpdateEvent event) {
-    if (Objects.equals(event.getColumn().getId(), COL_COSTS_PRICE)
-        && BeeUtils.isZero(BeeUtils.toDoubleOrNull(event.getNewValue()))) {
+  private void amountEntry(IsRow row) {
+    Totalizer totalizer = new Totalizer(getDataColumns());
 
-      event.consume();
-
-      amountEntry(DataUtils.getDouble(gridView.getDataColumns(), event.getRowValue(),
-          COL_COSTS_QUANTITY),
-          (price) -> {
-            event.setNewValue(price);
-            event.setConsumed(false);
-            gridView.fireEvent(event);
-          });
-      return;
-    }
-    super.onReadyForUpdate(gridView, event);
-  }
-
-  private static void amountEntry(Double qty, Consumer<String> amountConsumer) {
     InputNumber input = new InputNumber();
+    Double total = totalizer.getTotal(row);
 
-    Global.inputWidget(Localized.dictionary().amount(), input, new InputCallback() {
-      @Override
-      public String getErrorMessage() {
-        if (!BeeUtils.isPositive(input.getNumber())) {
-          return Localized.dictionary().valueRequired();
+    if (BeeUtils.isDouble(total)) {
+      input.setValue(BeeUtils.round(total, 2));
+    }
+    Global.inputWidget(Localized.dictionary().amount(), input, () -> {
+      Double amount = input.getNumber();
+      String price = null;
+
+      if (BeeUtils.isDouble(amount)) {
+        if (!totalizer.isVatInclusive(row)) {
+          row.clearCell(getDataIndex(TradeConstants.COL_TRADE_VAT_PLUS));
+          amount -= BeeUtils.unbox(totalizer.getVat(row, amount));
+          row.setValue(getDataIndex(TradeConstants.COL_TRADE_VAT_PLUS), 1);
         }
-        return InputCallback.super.getErrorMessage();
+        Double qty = row.getDouble(getDataIndex(COL_COSTS_QUANTITY));
+        price = BeeUtils.toString(amount / (BeeUtils.isZero(qty) ? 1 : qty), 5);
       }
+      List<BeeColumn> columns = new ArrayList<>();
+      List<String> oldValues = new ArrayList<>();
+      List<String> newValues = new ArrayList<>();
 
-      @Override
-      public void onSuccess() {
-        amountConsumer
-            .accept(BeeUtils.toString(input.getNumber() / (BeeUtils.isPositive(qty) ? qty : 1), 5));
+      columns.add(DataUtils.getColumn(COL_COSTS_PRICE, getDataColumns()));
+      oldValues.add(row.getString(getDataIndex(COL_COSTS_PRICE)));
+      newValues.add(price);
+
+      String oldCurrency = row.getString(getDataIndex(COL_COSTS_CURRENCY));
+      String newCurrency = null;
+
+      if (!BeeUtils.isEmpty(price)) {
+        newCurrency = BeeUtils.notEmpty(oldCurrency,
+            DataUtils.isId(ClientDefaults.getCurrency())
+                ? BeeUtils.toString(ClientDefaults.getCurrency()) : null);
       }
+      columns.add(DataUtils.getColumn(COL_COSTS_CURRENCY, getDataColumns()));
+      oldValues.add(oldCurrency);
+      newValues.add(newCurrency);
+
+      Queries.update(getViewName(), row.getId(), row.getVersion(), columns, oldValues, newValues,
+          null, new RowUpdateCallback(getViewName()));
     });
   }
 }
