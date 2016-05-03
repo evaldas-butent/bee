c560d7cd2d2fff4d8d9b8598ca225de18f80b3c5
Zilvinas Juraska <zilvinas.juraska@b-novo.com>
2016-05-03 16:38:07 +0300

    1/2 commit with constants, columns and dictionary values. Also values added to project.css to display filters nicely.

--- src/com/butent/bee/shared/i18n/Dictionary.java
+++ src/com/butent/bee/shared/i18n/Dictionary.java
@@ -3804,9 +3804,13 @@ public interface Dictionary {
 
   default String prjFilterAll() {return g("prjFilterAll");}
 
+  default String prjFilterNotLate() {return g("prjFilterNotLate");}
+
   default String prjIncomePlan() {return g("prjIncomePlan");}
 
   default String prjInitialStage() {return g("prjInitialStage");}
+
+  default String prjLabelNotLate() {return g("prjLabelNotLate");}
 
   default String prjManager() {return g("prjManager");}
 
@@ -3829,6 +3833,8 @@ public interface Dictionary {
   default String prjOtherExpenses() {return g("prjOtherExpenses");}
 
   default String prjOverdue() {return g("prjOverdue");}
+
+  default String prjOverduePercent() {return g("prjOverduePercent");}
 
   default String prjParticipant() {return g("prjParticipant");}
 
