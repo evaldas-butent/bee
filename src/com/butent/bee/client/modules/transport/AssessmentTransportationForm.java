b4d4b65b09b8fa18057313af1dad9208525ee4ac
Vidas <vidas@b-novo.com>
2016-05-05 18:34:08 +0300

    Fixes after merge

--- src/com/butent/bee/client/modules/transport/AssessmentTransportationForm.java
+++ src/com/butent/bee/client/modules/transport/AssessmentTransportationForm.java
@@ -1,13 +1,10 @@
 package com.butent.bee.client.modules.transport;
 
-import com.google.common.collect.Lists;
 import com.google.gwt.user.client.ui.Widget;
 
 import static com.butent.bee.shared.modules.transport.TransportConstants.*;
 
 import com.butent.bee.client.BeeKeeper;
-import com.butent.bee.client.UserInfo;
-import com.butent.bee.client.data.Data;
 import com.butent.bee.client.data.Queries;
 import com.butent.bee.client.data.Queries.RowSetCallback;
 import com.butent.bee.client.dom.DomUtils;
@@ -20,34 +17,24 @@ import com.butent.bee.client.view.form.FormView;
 import com.butent.bee.client.view.form.interceptor.AbstractFormInterceptor;
 import com.butent.bee.client.view.form.interceptor.FormInterceptor;
 import com.butent.bee.client.view.form.interceptor.PrintFormInterceptor;
-import com.butent.bee.shared.BeeConst;
 import com.butent.bee.shared.Pair;
 import com.butent.bee.shared.data.BeeRow;
 import com.butent.bee.shared.data.BeeRowSet;
 import com.butent.bee.shared.data.IsRow;
 import com.butent.bee.shared.data.filter.Filter;
-import com.butent.bee.shared.data.view.DataInfo;
 import com.butent.bee.shared.i18n.Dictionary;
 import com.butent.bee.shared.i18n.Localized;
-import com.butent.bee.shared.modules.classifiers.ClassifierConstants;
 import com.butent.bee.shared.time.DateTime;
 import com.butent.bee.shared.utils.BeeUtils;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public class AssessmentTransportationForm extends PrintFormInterceptor {
 
-  private static final String WIDGET_NAME_FIRST_PARAGRAPH = "firstParagraph";
-  private static final List<String> PARAGRAPH_TEXT = Lists.newArrayList("atstovaujama",
-      "toliau vadinamas \"Užsakovu\" ir", "atstovaujama", "________________________________",
-      ", toliau vadinamas \"Vežėju\" sudarė šią krovinio vežimo sutartį");
-
   private Widget totals;
   private Widget cargo;
-  private Widget firstParagraph;
   private Widget forwarderDetails;
   private Widget customerDetails;
 
@@ -173,7 +160,7 @@ public class AssessmentTransportationForm extends PrintFormInterceptor {
 
     forwarderDetails = new Flow();
     customerDetails = new Flow();
-    firstParagraph = new Flow();
+
     DomUtils.setDataProperty(forwarderDetails.getElement(), "locale", Localized.dictionary()
         .languageTag());
     DomUtils.setDataProperty(customerDetails.getElement(), "locale", Localized.dictionary()
@@ -182,7 +169,6 @@ public class AssessmentTransportationForm extends PrintFormInterceptor {
     ClassifierUtils.getCompanyInfo(row.getLong(form.getDataIndex(COL_FORWARDER)),
         forwarderDetails);
     ClassifierUtils.getCompanyInfo(BeeKeeper.getUser().getCompany(), customerDetails);
-    renderFirstParagraph(form, row, firstParagraph);
 
     super.beforeRefresh(form, row);
   }
@@ -206,9 +192,6 @@ public class AssessmentTransportationForm extends PrintFormInterceptor {
           widget.getElement().setInnerHTML(customerDetails.getElement().getInnerHTML());
         }
 
-        if (BeeUtils.same(name, WIDGET_NAME_FIRST_PARAGRAPH)) {
-          widget.getElement().setInnerText(firstParagraph.getElement().getInnerText());
-        }
         super.afterCreateWidget(name, widget, callback);
       }
 
@@ -222,53 +205,5 @@ public class AssessmentTransportationForm extends PrintFormInterceptor {
   @Override
   public FormInterceptor getInstance() {
     return new AssessmentTransportationForm();
-  }
-
-  private static void renderFirstParagraph(final FormView form, final IsRow row,
-      final Widget widget) {
-    final UserInfo info = BeeKeeper.getUser();
-    final DataInfo viewInfo = Data.getDataInfo(ClassifierConstants.VIEW_COMPANIES);
-    final String customerPersonSignature =
-        BeeUtils.joinWords(info.getFirstName(), info.getLastName());
-    long forwarderId = BeeUtils.unbox(row.getLong(form.getDataIndex(COL_FORWARDER)));
-    Filter filter = Filter.idIn(Lists.newArrayList(info.getCompany(), forwarderId));
-
-    Queries.getRowSet(ClassifierConstants.VIEW_COMPANIES, viewInfo.getColumnNames(false),
-        filter, new RowSetCallback() {
-
-          @Override
-          public void onSuccess(BeeRowSet result) {
-            String customerCompanySignature = "";
-            String forwarderCompanySignature = "";
-
-            for (IsRow resutlRow : result) {
-              long id = resutlRow.getId();
-
-              String signature = BeeUtils.joinWords(resutlRow.getString(viewInfo.getColumnIndex(
-                  ClassifierConstants.COL_COMPANY_NAME))
-                  + BeeConst.STRING_COMMA, resutlRow.getString(viewInfo.getColumnIndex(
-                  ClassifierConstants.ALS_COMPANY_TYPE_NAME)));
-
-              if (id == BeeUtils.unbox(info.getCompany())) {
-                customerCompanySignature = signature;
-              } else if (id == BeeUtils.unbox(row.getLong(form.getDataIndex(COL_FORWARDER)))) {
-                forwarderCompanySignature = signature;
-              }
-
-            }
-
-            String pText = BeeUtils.joinWords(customerCompanySignature + BeeConst.STRING_COMMA,
-                BeeUtils.join(BeeConst.STRING_SPACE, PARAGRAPH_TEXT.subList(0, 1)),
-                customerPersonSignature + BeeConst.STRING_COMMA,
-                BeeUtils.join(BeeConst.STRING_SPACE, PARAGRAPH_TEXT.subList(1, 2)),
-                forwarderCompanySignature + BeeConst.STRING_COMMA,
-                BeeUtils.join(BeeConst.STRING_SPACE, PARAGRAPH_TEXT.subList(2, PARAGRAPH_TEXT
-                    .size()))
-            );
-
-            widget.getElement().setInnerText(pText);
-          }
-        });
-
   }
 }
