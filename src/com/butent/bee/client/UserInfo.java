63185d7c6d7daddc7f5c0a5610d043b755ff8d67
Ernesta Kebelyte <ernesta.kebelyte@b-novo.com>
2016-05-20 13:58:12 +0300

    Removed empty lines, changed label name, Created new field in user settings. Field will be used then user receive new chat message.

--- src/com/butent/bee/client/UserInfo.java
+++ src/com/butent/bee/client/UserInfo.java
@@ -47,6 +47,7 @@ public class UserInfo implements HasInfo {
   private BeeRowSet settings;
 
   private boolean openInNewTab;
+  private boolean showNewMessagesNotifier;
 
   private int clickSensitivityMillis;
   private int clickSensitivityDistance;
@@ -445,6 +446,10 @@ public class UserInfo implements HasInfo {
     return styleId;
   }
 
+  public boolean showNewMessagesNotifier() {
+    return showNewMessagesNotifier;
+  }
+
   private void setClickSensitivityDistance(int clickSensitivityDistance) {
     this.clickSensitivityDistance = clickSensitivityDistance;
   }
@@ -475,6 +480,7 @@ public class UserInfo implements HasInfo {
 
   private void updateFields() {
     setOpenInNewTab(getBooleanSetting(COL_OPEN_IN_NEW_TAB));
+    setShowNewMessagesNotifier(getBooleanSetting(COL_SHOW_NEW_MESSAGES_NOTIFIER));
 
     setClickSensitivityMillis(getIntSetting(COL_CLICK_SENSITIVITY_MILLIS, BeeConst.UNDEF));
     setClickSensitivityDistance(getIntSetting(COL_CLICK_SENSITIVITY_DISTANCE, BeeConst.UNDEF));
@@ -485,5 +491,9 @@ public class UserInfo implements HasInfo {
 
   private void updateStyle(String css) {
     DomUtils.setText(getStyleId(), css);
+  }
+
+  public void setShowNewMessagesNotifier(boolean showNewMessagesNotifier) {
+    this.showNewMessagesNotifier = showNewMessagesNotifier;
   }
 }
