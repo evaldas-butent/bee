1e2fcd52d9ad0f363de6a92a08dcaaf01ec54e48
Evaldas <evaldas.bagatavicius@butent.lt>
2016-05-23 13:01:10 +0300

    External GWT/JBOSS configuration tools for linux IDE's

--- samples/cli/createjdbc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-AS_ADMIN=asadmin
-# AS_ADMIN=/opt/glassfish4/glassfish/bin/asadmin
-
-$AS_ADMIN create-jdbc-connection-pool --datasourceclassname org.postgresql.ds.PGSimpleDataSource --restype javax.sql.DataSource --property serverName=192.168.0.240:user=postgres:password=admin:databaseName=bee PgPool
-$AS_ADMIN create-jdbc-resource --connectionpoolid PgPool jdbc/BeeDS
-$AS_ADMIN ping-connection-pool PgPool
