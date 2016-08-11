andrius-butent/mybee

Task 12601. Created reminder dialog. Used in task form.

src/com/butent/bee/shared/i18n/Dictionary.java
@@ -142,6 +142,8 @@ public interface Dictionary {
 
   default String advance() {return g("advance");}
 
+  default String after() {return g("after");}
+
   default String allValuesCannotBeEmpty() {return g("allValuesCannotBeEmpty");}
 
   default String allValuesEmpty(Object p0, Object p1) {
@@ -218,6 +220,8 @@ public interface Dictionary {
   default String banksAccounts() {return g("banksAccounts");}
 
   default String baseUnit() {return g("baseUnit");}
+
+  default String before() {return g("before");}
 
   default String birthdaysParties() {return g("birthdaysParties");}
 
@@ -942,11 +946,13 @@ public interface Dictionary {
   default String crmMailAssignedTasks() {return g("crmMailAssignedTasks");}
 
   default String crmMailTaskSubject() {return g("crmMailTaskSubject");}
-  
+
+  default String crmMailTasksSummary() {return g("crmMailTasksSummary");}
+
   default String crmMailTasksSummarySubject() {return g("crmMailTasksSummarySubject");}
-  
+
   default String crmMailTasksSummaryText() {return g("crmMailTasksSummaryText");}
-  
+
   default String crmNewDurationType() {return g("crmNewDurationType");}
 
   default String crmNewRecurringTask() {return g("crmNewRecurringTask");}
@@ -1186,7 +1192,7 @@ public interface Dictionary {
   default String crmTaskForwarding() {return g("crmTaskForwarding");}
 
   default String crmTaskForwardingForExecution() {return g("crmTaskForwardingForExecution");}
-  
+
   default String crmTaskLabelDelayedHours() {return g("crmTaskLabelDelayedHours");}
 
   default String crmTaskLabelLate() {return g("crmTaskLabelLate");}
@@ -4027,7 +4033,13 @@ public interface Dictionary {
 
   default String reminderCaption() {return g("reminderCaption");}
 
+  default String reminderDataField() {return g("reminderDataField");}
+
+  default String reminderDataIndicator() {return g("reminderDataIndicator");}
+
   default String reminderMethod() {return g("reminderMethod");}
+
+  default String reminderModule() {return g("reminderModule");}
 
   default String reminderName() {return g("reminderName");}
 
@@ -5757,6 +5769,24 @@ public interface Dictionary {
   default String userProfile() {return g("userProfile");}
 
   default String userProperties() {return g("userProperties");}
+
+  default String userRemind() {return g("userRemind");}
+
+  default String userReminder() {return g("userReminder");}
+
+  default String userReminderCancel() {return g("userReminderCancel");}
+
+  default String userReminderDataLabel() {return g("userReminderDataLabel");}
+
+  default String userReminderOtherTime() {return g("userReminderOtherTime");}
+
+  default String userReminderSendRemind() {return g("userReminderSendRemind");}
+
+  default String userReminderSendRemindDateError() {return g("userReminderSendRemindDateError");}
+
+  default String userReminderSuspend() {return g("userReminderSuspend");}
+
+  default String userReminderUpdate() {return g("userReminderUpdate");}
 
   default String userRoles() {return g("userRoles");}
 

src/com/butent/bee/shared/modules/administration/AdministrationConstants.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import com.butent.bee.shared.Service;
 import com.butent.bee.shared.data.BeeColumn;
 import com.butent.bee.shared.data.value.ValueType;
+import com.butent.bee.shared.i18n.Dictionary;
 import com.butent.bee.shared.i18n.Localized;
 import com.butent.bee.shared.i18n.SupportedLocale;
 import com.butent.bee.shared.imports.ImportType;
@@ -16,6 +17,7 @@ import com.butent.bee.shared.rights.RightsState;
 import com.butent.bee.shared.time.ScheduleDateMode;
 import com.butent.bee.shared.time.WorkdayTransition;
 import com.butent.bee.shared.ui.HasCaption;
+import com.butent.bee.shared.ui.HasLocalizedCaption;
 import com.butent.bee.shared.ui.UserInterface;
 import com.butent.bee.shared.utils.EnumUtils;
 
@@ -40,6 +42,38 @@ public final class AdministrationConstants {
     @Override
     public String getCaption() {
       return this.name().toLowerCase();
+    }
+  }
+
+  public enum ReminderDataField implements HasLocalizedCaption {
+    START_DATE {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.startingDate();
+      }
+    },
+
+    END_DATE {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.endingDate();
+      }
+    }
+  }
+
+  public enum ReminderDataIndicator implements HasLocalizedCaption {
+    BEFORE {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.before();
+      }
+    },
+
+    AFTER {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.after();
+      }
     }
   }
 
@@ -75,6 +109,9 @@ public final class AdministrationConstants {
     EnumUtils.register(ImportType.class);
     EnumUtils.register(Module.class);
     EnumUtils.register(SysObject.class);
+
+    EnumUtils.register(ReminderDataField.class);
+    EnumUtils.register(ReminderDataIndicator.class);
   }
 
   public static final String FILE_URL = "file";
@@ -219,6 +256,8 @@ public final class AdministrationConstants {
 
   public static final String VIEW_UI_THEMES = "UiThemes";
 
+  public static final String VIEW_USER_REMINDERS = "UserReminders";
+
   public static final String GRID_HISTORY = "History";
   public static final String GRID_USER_GROUP_MEMBERS = "UserGroupMembers";
 
@@ -272,6 +311,9 @@ public final class AdministrationConstants {
   public static final String COL_REMINDER_MINUTES = "Minutes";
   public static final String COL_REMINDER_TEMPLATE_CAPTION = "Caption";
   public static final String COL_REMINDER_TEMPLATE = "Template";
+  public static final String COL_REMINDER_MODULE = "Module";
+  public static final String COL_REMINDER_DATA_FIELD = "ReminderDataField";
+  public static final String COL_REMINDER_DATA_INDICATOR = "ReminderDataIndicator";
 
   public static final String COL_COLOR = "Color";
   public static final String COL_COLOR_NAME = "Name";
@@ -360,6 +402,13 @@ public final class AdministrationConstants {
   public static final String COL_EVENT_RESULT = "Result";
 
   public static final String COL_DICTIONARY_KEY = "Key";
+
+  public static final String COL_USER_REMINDER_OBJECT = "Object";
+  public static final String COL_USER_REMINDER_OBJECT_MODULE = "ObjectModule";
+  public static final String COL_USER_REMINDER_USER = "User";
+  public static final String COL_USER_REMINDER_TYPE = "ReminderType";
+  public static final String COL_USER_REMINDER_ACTIVE = "Active";
+  public static final String COL_USER_REMINDER_TIME = "ReminderTime";
 
   public static final String ALS_FILE_NAME = "FileName";
   public static final String ALS_FILE_SIZE = "FileSize";

evaldas-butent/bee

src/com/butent/bee/shared/i18n/Dictionary.java
@@ -142,6 +142,8 @@ public interface Dictionary {
 
   default String advance() {return g("advance");}
 
+  default String after() {return g("after");}
+
   default String allValuesCannotBeEmpty() {return g("allValuesCannotBeEmpty");}
 
   default String allValuesEmpty(Object p0, Object p1) {
@@ -230,6 +232,8 @@ public interface Dictionary {
   default String banksAccounts() {return g("banksAccounts");}
 
   default String baseUnit() {return g("baseUnit");}
+
+  default String before() {return g("before");}
 
   default String birthdaysParties() {return g("birthdaysParties");}
 
@@ -4183,7 +4187,13 @@ public interface Dictionary {
 
   default String reminderCaption() {return g("reminderCaption");}
 
+  default String reminderDateField() {return g("reminderDateField");}
+
+  default String reminderDateIndicator() {return g("reminderDateIndicator");}
+
   default String reminderMethod() {return g("reminderMethod");}
+
+  default String reminderModule() {return g("reminderModule");}
 
   default String reminderName() {return g("reminderName");}
 
@@ -5949,6 +5959,28 @@ public interface Dictionary {
   default String userProfile() {return g("userProfile");}
 
   default String userProperties() {return g("userProperties");}
+
+  default String userRemind() {return g("userRemind");}
+
+  default String userReminder() {return g("userReminder");}
+
+  default String userReminderCancel() {return g("userReminderCancel");}
+
+  default String userReminderCreated() {return g("userReminderCreated");}
+
+  default String userReminderDataLabel() {return g("userReminderDataLabel");}
+
+  default String userReminderDisabled() {return g("userReminderDisabled");}
+
+  default String userReminderOtherTime() {return g("userReminderOtherTime");}
+
+  default String userReminderSendRemind() {return g("userReminderSendRemind");}
+
+  default String userReminderSendRemindDateError() {return g("userReminderSendRemindDateError");}
+
+  default String userReminderSuspend() {return g("userReminderSuspend");}
+
+  default String userReminderUpdate() {return g("userReminderUpdate");}
 
   default String userRoles() {return g("userRoles");}
 

src/com/butent/bee/shared/modules/administration/AdministrationConstants.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import com.butent.bee.shared.Service;
 import com.butent.bee.shared.data.BeeColumn;
 import com.butent.bee.shared.data.value.ValueType;
+import com.butent.bee.shared.i18n.Dictionary;
 import com.butent.bee.shared.i18n.Localized;
 import com.butent.bee.shared.i18n.SupportedLocale;
 import com.butent.bee.shared.imports.ImportType;
@@ -16,6 +17,7 @@ import com.butent.bee.shared.rights.RightsState;
 import com.butent.bee.shared.time.ScheduleDateMode;
 import com.butent.bee.shared.time.WorkdayTransition;
 import com.butent.bee.shared.ui.HasCaption;
+import com.butent.bee.shared.ui.HasLocalizedCaption;
 import com.butent.bee.shared.ui.UserInterface;
 import com.butent.bee.shared.utils.EnumUtils;
 
@@ -40,6 +42,38 @@ public final class AdministrationConstants {
     @Override
     public String getCaption() {
       return this.name().toLowerCase();
+    }
+  }
+
+  public enum ReminderDateField implements HasLocalizedCaption {
+    START_DATE {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.startingDate();
+      }
+    },
+
+    END_DATE {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.endingDate();
+      }
+    }
+  }
+
+  public enum ReminderDateIndicator implements HasLocalizedCaption {
+    BEFORE {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.before();
+      }
+    },
+
+    AFTER {
+      @Override
+      public String getCaption(Dictionary constants) {
+        return constants.after();
+      }
     }
   }
 
@@ -75,6 +109,9 @@ public final class AdministrationConstants {
     EnumUtils.register(ImportType.class);
     EnumUtils.register(Module.class);
     EnumUtils.register(SysObject.class);
+
+    EnumUtils.register(ReminderDateField.class);
+    EnumUtils.register(ReminderDateIndicator.class);
   }
 
   public static final String FILE_URL = "file";
@@ -219,6 +256,8 @@ public final class AdministrationConstants {
 
   public static final String VIEW_UI_THEMES = "UiThemes";
 
+  public static final String VIEW_USER_REMINDERS = "UserReminders";
+
   public static final String GRID_HISTORY = "History";
   public static final String GRID_USER_GROUP_MEMBERS = "UserGroupMembers";
 
@@ -272,6 +311,9 @@ public final class AdministrationConstants {
   public static final String COL_REMINDER_MINUTES = "Minutes";
   public static final String COL_REMINDER_TEMPLATE_CAPTION = "Caption";
   public static final String COL_REMINDER_TEMPLATE = "Template";
+  public static final String COL_REMINDER_MODULE = "Module";
+  public static final String COL_REMINDER_DATA_FIELD = "ReminderDateField";
+  public static final String COL_REMINDER_DATA_INDICATOR = "ReminderDateIndicator";
 
   public static final String COL_COLOR = "Color";
   public static final String COL_COLOR_NAME = "Name";
@@ -362,6 +404,14 @@ public final class AdministrationConstants {
   public static final String COL_EVENT_RESULT = "Result";
 
   public static final String COL_DICTIONARY_KEY = "Key";
+
+  public static final String COL_USER_REMINDER_OBJECT = "Object";
+  public static final String COL_USER_REMINDER_OBJECT_MODULE = "ObjectModule";
+  public static final String COL_USER_REMINDER_USER = "User";
+  public static final String COL_USER_REMINDER_TYPE = "ReminderType";
+  public static final String COL_USER_REMINDER_ACTIVE = "Active";
+  public static final String COL_USER_REMINDER_TIMEOUT = "Timeout";
+  public static final String COL_USER_REMINDER_TIME = "ReminderTime";
 
   public static final String ALS_FILE_NAME = "FileName";
   public static final String ALS_FILE_SIZE = "FileSize";