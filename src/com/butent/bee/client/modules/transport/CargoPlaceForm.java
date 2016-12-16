c992a9ba729584db196f630f9757b48268691a91
ernesta-bnovo <ernesta.kebelyte@b-novo.com>
2016-12-16 15:26:39 +0200

    Task 23588 Autocomplete added in Company and Address fields for Cargo… (#1014)
    * Task 23588 Autocomplete added in Company and Address fields for CargoLoading/Unloading table.
    
    * Task 23588 Autocomplete added in Company and Address fields for CargoLoading/Unloading table.

--- src/com/butent/bee/client/modules/transport/CargoPlaceForm.java
+++ src/com/butent/bee/client/modules/transport/CargoPlaceForm.java
@@ -2,13 +2,29 @@ package com.butent.bee.client.modules.transport;
 
 import static com.butent.bee.shared.modules.transport.TransportConstants.*;
 
+import com.butent.bee.client.composite.DataSelector;
+import com.butent.bee.client.ui.FormFactory;
+import com.butent.bee.client.ui.IdentifiableWidget;
 import com.butent.bee.client.view.form.interceptor.AbstractFormInterceptor;
 import com.butent.bee.client.view.form.interceptor.FormInterceptor;
 import com.butent.bee.shared.i18n.Localized;
+import com.butent.bee.shared.utils.BeeUtils;
 
 import java.util.Objects;
 
 public class CargoPlaceForm extends AbstractFormInterceptor {
+
+  @Override
+  public void afterCreateWidget(String name, IdentifiableWidget widget,
+      FormFactory.WidgetDescriptionCallback callback) {
+    super.afterCreateWidget(name, widget, callback);
+    String widgetSuffix = BeeUtils.removePrefix(getViewName(), COL_CARGO);
+
+    if (widget instanceof DataSelector && !BeeUtils.isEmpty(name) && !BeeUtils.isEmpty(widgetSuffix)
+        && !name.endsWith(widgetSuffix)) {
+      ((DataSelector) widget).setVisible(false);
+    }
+  }
 
   @Override
   public String getCaption() {
