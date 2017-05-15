3aa8820dff540f8b00c7883aeb580fb4eebf3581
andrius-butent <andrius.antipovas@butent.lt>
2017-05-15 11:19:50 +0300

    Test fixes (#1351)

--- test/com/butent/bee/shared/testutils/TestWildcards.java
+++ test/com/butent/bee/shared/testutils/TestWildcards.java
@@ -3,6 +3,7 @@ package com.butent.bee.shared.testutils;
 import com.butent.bee.shared.utils.Wildcards;
 import com.butent.bee.shared.utils.Wildcards.Pattern;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
@@ -293,5 +294,44 @@ public class TestWildcards {
     assertEquals(Wildcards.isSqlCaseSensitive(), true);
     Wildcards.setSqlCaseSensitivity(false);
     assertEquals(Wildcards.isSqlCaseSensitive(), false);
+  }
+
+  @Test
+  public final void testGetExpr() {
+    Pattern a = Wildcards.getPattern("File*.txt=5", '*', '\0', false, null);
+
+    assertEquals("File*.txt=5", a.getExpr());
+
+    a = Wildcards.getPattern("     File*.txt=5   \t \t \t \r ", '*', '\0', false, null);
+    assertEquals("File*.txt=5", a.getExpr());
+  }
+
+  @Test
+  public final void testGetTokens() {
+    Pattern a = Wildcards.getPattern("File*.txt=5", '*', '\0', false, null);
+
+    String[] b = {"File", "*", ".txt=5"};
+    assertArrayEquals(b, a.getTokens());
+
+    a = Wildcards.getPattern("File*.txt=5", '*', '.', false, null);
+    String[] c = {"File", ".", "*", "txt=5"};
+    assertArrayEquals(c, a.getTokens());
+
+    assertEquals("File*.txt=5", a.toString());
+
+    a = Wildcards.getPattern("File*.txt=5", '*', '.', true, null);
+    assertEquals("File*.txt=5 (sensitive)", a.toString());
+
+    a = Wildcards.getPattern("File*.txt5=", ';', '>', true, '=');
+    assertEquals("File*.txt5 (sensitive) (exact)", a.toString());
+
+    a = Wildcards.getPattern("File*.txt5", ';', '>', true, null);
+    assertEquals("File*.txt5 (sensitive)", a.toString());
+
+    a = Wildcards.getPattern("abcd>efgh>opkg<asdda>>>", '>', '<', false, null);
+    assertEquals("abcd>efgh>opkg<asdda>>>", a.toString());
+
+    Object[] mas = {"abcd", ">", "efgh", ">", "opkg", "<", "asdda", ">"};
+    assertArrayEquals(mas, a.getTokens());
   }
 }
