757a072bccebdb4095fb24adbf5e6ebd8a8338e9
Vidas <vidas@b-novo.com>
2017-08-29 13:20:03 +0300

    v2.153

--- war/js/settings.js
+++ war/js/settings.js
@@ -1,9 +1,9 @@
 var BeeSettings = {
   "appName": "B-NOVO",
-  "version": "2.152",
+  "version": "2.153",
   "licence": "",
   "build": "4892",
-  "releaseDate": "2017.08.03",
+  "releaseDate": "2017.08.29",
   "logoImage": "images/logo_white.png",
   "logoOpen": "http://www.b-novo.lt",
   "providerSensitivityMillis": 300,
