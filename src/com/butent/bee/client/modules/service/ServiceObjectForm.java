ccb10ca0726f40d4ecda6fc533c122070302e899
Andrius <andrius.antipovas@b-novo.com>
2018-01-11 22:03:30 +0200

    test

--- src/com/butent/bee/client/modules/service/ServiceObjectForm.java
+++ src/com/butent/bee/client/modules/service/ServiceObjectForm.java
@@ -50,15 +50,7 @@ import com.butent.bee.client.view.grid.interceptor.AbstractGridInterceptor;
 import com.butent.bee.client.view.grid.interceptor.GridInterceptor;
 import com.butent.bee.client.widget.Label;
 import com.butent.bee.shared.Holder;
-import com.butent.bee.shared.State;
-import com.butent.bee.shared.communication.ResponseObject;
 import com.butent.bee.shared.css.values.TextAlign;
-import com.butent.bee.shared.data.BeeColumn;
-import com.butent.bee.shared.data.BeeRow;
-import com.butent.bee.shared.data.BeeRowSet;
-import com.butent.bee.shared.data.DataUtils;
-import com.butent.bee.shared.data.IsRow;
-import com.butent.bee.shared.data.RelationUtils;
 import com.butent.bee.shared.data.event.DataChangeEvent;
 import com.butent.bee.shared.data.event.RowUpdateEvent;
 import com.butent.bee.shared.data.filter.Filter;
@@ -72,9 +64,6 @@ import com.butent.bee.shared.utils.BeeUtils;
 
 import java.util.*;
 import java.util.Map.Entry;
-import java.util.Objects;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class ServiceObjectForm extends MaintenanceExpanderForm implements ClickHandler,
     RowActionEvent.Handler, SelectorEvent.Handler, RowUpdateEvent.Handler, DataChangeEvent.Handler {
@@ -209,36 +198,7 @@ public class ServiceObjectForm extends MaintenanceExpanderForm implements ClickH
 
   @Override
   public void onClick(ClickEvent event) {
-    Dictionary loc = Localized.dictionary();
-
-    Global.inputCollection(loc.mainCriteria(), loc.name(), true, criteriaEditors.keySet(),
-        new Consumer<Collection<String>>() {
-          @Override
-          public void accept(Collection<String> collection) {
-            Map<String, Editor> oldCriteria = new HashMap<>(criteriaEditors);
-            criteriaEditors.clear();
-
-            for (String crit : collection) {
-              Editor input = oldCriteria.get(crit);
-
-              if (input == null) {
-                input = createAutocomplete(VIEW_SERVICE_DISTINCT_VALUES,
-                    COL_SERVICE_CRITERION_VALUE, crit);
-              }
-              criteriaEditors.put(crit, input);
-            }
-            render();
-          }
-        },
-        new Function<String, Editor>() {
-          @Override
-          public Editor apply(String value) {
-            Editor editor = createAutocomplete(VIEW_SERVICE_DISTINCT_CRITERIA,
-                COL_SERVICE_CRITERION_NAME, null);
-            editor.setValue(value);
-            return editor;
-          }
-        });
+    renderMainCriteria();
   }
 
   @Override
@@ -262,30 +222,27 @@ public class ServiceObjectForm extends MaintenanceExpanderForm implements ClickH
         && BeeUtils.isTrue(getActiveRow().getBoolean(getDataIndex(COL_COMPANY_TYPE_PERSON)))) {
       Long companyId = getActiveRow().getLong(getDataIndex(COL_SERVICE_CUSTOMER));
       Queries.getRow(VIEW_COMPANY_PERSONS, Filter.equals(COL_COMPANY, companyId), null,
-          new RowCallback() {
-            @Override
-            public void onSuccess(BeeRow companyPersonRow) {
-              if (companyPersonRow != null && DataUtils.isId(companyPersonRow.getId())) {
-                DataInfo targetDataInfo = Data.getDataInfo(getViewName());
-                DataInfo sourceDataInfo = Data.getDataInfo(VIEW_COMPANY_PERSONS);
+              companyPersonRow -> {
+                if (companyPersonRow != null && DataUtils.isId(companyPersonRow.getId())) {
+                  DataInfo targetDataInfo = Data.getDataInfo(getViewName());
+                  DataInfo sourceDataInfo = Data.getDataInfo(VIEW_COMPANY_PERSONS);
 
-                getActiveRow().setValue(targetDataInfo.getColumnIndex(ALS_CONTACT_PERSON),
-                    companyPersonRow.getId());
-                RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_PHONE, getActiveRow(),
-                    sourceDataInfo, COL_PHONE, companyPersonRow);
-                RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_FIRST_NAME,
-                    getActiveRow(), sourceDataInfo, COL_FIRST_NAME, companyPersonRow);
-                RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_LAST_NAME,
-                    getActiveRow(), sourceDataInfo, COL_LAST_NAME, companyPersonRow);
-                RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_EMAIL, getActiveRow(),
-                    sourceDataInfo, COL_EMAIL, companyPersonRow);
-                RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_ADDRESS, getActiveRow(),
-                    sourceDataInfo, COL_ADDRESS, companyPersonRow);
+                  getActiveRow().setValue(targetDataInfo.getColumnIndex(ALS_CONTACT_PERSON),
+                      companyPersonRow.getId());
+                  RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_PHONE, getActiveRow(),
+                      sourceDataInfo, COL_PHONE, companyPersonRow);
+                  RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_FIRST_NAME,
+                      getActiveRow(), sourceDataInfo, COL_FIRST_NAME, companyPersonRow);
+                  RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_LAST_NAME,
+                      getActiveRow(), sourceDataInfo, COL_LAST_NAME, companyPersonRow);
+                  RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_EMAIL, getActiveRow(),
+                      sourceDataInfo, COL_EMAIL, companyPersonRow);
+                  RelationUtils.maybeUpdateColumn(targetDataInfo, ALS_CONTACT_ADDRESS, getActiveRow(),
+                      sourceDataInfo, COL_ADDRESS, companyPersonRow);
 
-                getFormView().refreshBySource(ALS_CONTACT_PERSON);
-              }
-            }
-          });
+                  getFormView().refreshBySource(ALS_CONTACT_PERSON);
+                }
+              });
     }
   }
 
@@ -440,6 +397,111 @@ public class ServiceObjectForm extends MaintenanceExpanderForm implements ClickH
     }
   }
 
+  private void renderMainCriteria() {
+
+    Queries.getRowSet(VIEW_SERVICE_MAIN_CRITERIA, null, null, result -> {
+
+      if (result.isEmpty()) {
+        return;
+      }
+
+      Dictionary d = Localized.dictionary();
+      Latch rNo = new Latch(0);
+
+      HtmlTable table = new HtmlTable(StyleUtils.NAME_INFO_TABLE);
+      table.getRowFormatter().addStyleName(rNo.get(), StyleUtils.className(FontWeight.BOLD));
+      table.getRowFormatter().addStyleName(rNo.get(), StyleUtils.className(TextAlign.CENTER));
+
+      CheckBox checkAll = new CheckBox();
+      Map<Long, CheckBox> checkBoxMap = new HashMap<>();
+
+      checkAll.addValueChangeHandler(valueChangeEvent -> {
+        for (CheckBox cb : checkBoxMap.values()) {
+          cb.setValue(valueChangeEvent.getValue());
+        }
+      });
+
+      int c = 0;
+      table.setWidget(rNo.get(), c++, checkAll);
+      table.setText(rNo.get(), c++, d.captionId());
+      table.setText(rNo.get(), c++, d.ordinal());
+      table.setText(rNo.get(), c++, d.criterionName());
+
+      for (BeeRow row : result) {
+        rNo.increment();
+        int cNo = 0;
+
+        CheckBox cb = new CheckBox();
+
+        cb.setValue(criteriaList.contains(row.getId()));
+        checkBoxMap.put(row.getId(), cb);
+
+        table.setWidget(rNo.get(), cNo++, cb);
+        table.setText(rNo.get(), cNo++, BeeUtils.toString(row.getId()));
+        table.setText(rNo.get(), cNo++, row.getString(0));
+        table.setText(rNo.get(), cNo++, row.getString(1));
+      }
+
+      StyleUtils.setOverflow(table, StyleUtils.ScrollBars.VERTICAL, Overflow.AUTO);
+
+      Global.inputWidget(d.mainCriteria(), table, () -> {
+
+        BeeRowSet rowSet = new BeeRowSet(VIEW_SERVICE_OBJECT_MAIN_CRITERIA,
+          Data.getColumns(VIEW_SERVICE_OBJECT_MAIN_CRITERIA,
+            Arrays.asList(COL_SERVICE_OBJECT, COL_SERVICE_MAIN_CRITERIA)));
+
+        List<Long> delete = new ArrayList<>();
+
+        int actions = 0;
+
+        for (Entry<Long, CheckBox> entry : checkBoxMap.entrySet()) {
+          if (entry.getValue().isChecked() && !criteriaList.contains(entry.getKey())) {
+            BeeRow row = rowSet.addEmptyRow();
+            row.setValue(0, getActiveRowId());
+            row.setValue(1, entry.getKey());
+
+          } else if (!entry.getValue().isChecked() && criteriaList.contains(entry.getKey())) {
+            delete.add(entry.getKey());
+          }
+        }
+
+        if (delete.size() > 0) {
+          actions++;
+        }
+        if (rowSet.getNumberOfRows() > 0) {
+          actions++;
+        }
+
+        insertDeleteData(rowSet, delete, actions);
+      });
+    });
+  }
+
+  private void insertDeleteData(BeeRowSet rowSet, List<Long> delete, int actions) {
+    if (actions == 0) {
+      return;
+    }
+
+    Runnable runnable = new Runnable() {
+      int index;
+      @Override
+      public void run() {
+        if (Objects.equals(actions, ++index)) {
+          requery(getActiveRow());
+        }
+      }
+    };
+
+    if (delete.size() > 0) {
+      Queries.delete(VIEW_SERVICE_OBJECT_MAIN_CRITERIA, Filter.and(Filter.equals(COL_SERVICE_OBJECT, getActiveRowId()),
+        Filter.any(COL_SERVICE_MAIN_CRITERIA, delete)), result -> runnable.run());
+    }
+
+    if (rowSet.getNumberOfRows() > 0) {
+      Queries.insertRows(rowSet, result -> runnable.run());
+    }
+  }
+
   private void requery(IsRow row) {
     criteriaHistory.clear();
     criteriaEditors.clear();
@@ -455,29 +517,28 @@ public class ServiceObjectForm extends MaintenanceExpanderForm implements ClickH
 
     Filter filter = Filter.equals(COL_SERVICE_OBJECT, objId);
 
-    Queries.getRowSet(VIEW_SERVICE_OBJECT_CRITERIA, null, filter, new RowSetCallback() {
-      @Override
-      public void onSuccess(BeeRowSet result) {
-        if (result.getNumberOfRows() > 0) {
-          criteriaGroupId = result.getRow(0).getId();
+    Queries.getRowSet(VIEW_SERVICE_OBJECT_MAIN_CRITERIA, null, filter, result -> {
+      if (result.getNumberOfRows() > 0) {
 
-          for (BeeRow crit : result) {
-            String name = DataUtils.getString(result, crit, COL_SERVICE_CRITERION_NAME);
+        for (BeeRow crit : result) {
+          String name = DataUtils.getString(result, crit, COL_SERVICE_MAIN_CRITERIA + "Name");
 
           if (!BeeUtils.isEmpty(name)) {
             String value = DataUtils.getString(result, crit, COL_SERVICE_CRITERION_VALUE);
 
-              Autocomplete box = createAutocomplete(VIEW_SERVICE_DISTINCT_VALUES,
-                  COL_SERVICE_CRITERION_VALUE, name);
+            criteriaList.add(DataUtils.getLong(result, crit, COL_SERVICE_MAIN_CRITERIA));
+
+            Autocomplete box = createAutocomplete(VIEW_SERVICE_OBJECT_MAIN_CRITERIA,
+                COL_SERVICE_CRITERION_VALUE);
 
             box.setValue(value);
 
             criteriaHistory.put(name, value);
             criteriaEditors.put(name, box);
 
-              criteriaIds.put(name, DataUtils.getLong(result, crit, "ID"));
-            }
+            criteriaIds.put(name, crit.getId());
           }
+        }
 
         render();
       }
@@ -527,61 +588,11 @@ public class ServiceObjectForm extends MaintenanceExpanderForm implements ClickH
       }
     };
 
-    if (!BeeUtils.isEmpty(newValues)) {
-      final Consumer<Long> consumer = new Consumer<Long>() {
-        @Override
-        public void accept(Long id) {
-          List<BeeColumn> columns = Data.getColumns(VIEW_SERVICE_CRITERIA,
-              Lists.newArrayList(COL_SERVICE_CRITERIA_GROUP,
-                  COL_SERVICE_CRITERION_NAME, COL_SERVICE_CRITERION_VALUE));
-
-          for (Entry<String, String> entry : newValues.entrySet()) {
-            List<String> values = Lists.newArrayList(BeeUtils.toString(id),
-                entry.getKey(), entry.getValue());
-
-            Queries.insert(VIEW_SERVICE_CRITERIA, columns, values, null, new RowCallback() {
-              @Override
-              public void onSuccess(BeeRow result) {
-                scheduler.execute();
-              }
-            });
-          }
-        }
-      };
-
-      if (!DataUtils.isId(criteriaGroupId)) {
-        Queries.insert(VIEW_SERVICE_CRITERIA_GROUPS,
-            Data.getColumns(VIEW_SERVICE_CRITERIA_GROUPS, Lists.newArrayList(COL_SERVICE_OBJECT)),
-            Lists.newArrayList(BeeUtils.toString(row.getId())), null, new RowCallback() {
-              @Override
-              public void onSuccess(BeeRow result) {
-                consumer.accept(result.getId());
-              }
-            });
-      } else {
-        consumer.accept(criteriaGroupId);
-      }
-    }
-
     if (!BeeUtils.isEmpty(changedValues)) {
       for (Entry<Long, String> entry : changedValues.entrySet()) {
-        Queries.update(VIEW_SERVICE_CRITERIA, Filter.compareId(entry.getKey()),
-            COL_SERVICE_CRITERION_VALUE, new TextValue(entry.getValue()), new IntCallback() {
-              @Override
-              public void onSuccess(Integer result) {
-                scheduler.execute();
-              }
-            });
+        Queries.update(VIEW_SERVICE_OBJECT_MAIN_CRITERIA, Filter.compareId(entry.getKey()),
+            COL_SERVICE_CRITERION_VALUE, new TextValue(entry.getValue()), result -> scheduler.execute());
       }
-    }
-
-    if (!flt.isEmpty()) {
-      Queries.delete(VIEW_SERVICE_CRITERIA, flt, new IntCallback() {
-        @Override
-        public void onSuccess(Integer result) {
-          scheduler.execute();
-        }
-      });
     }
 
     return true;
